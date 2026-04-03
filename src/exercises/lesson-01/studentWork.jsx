63bc955335339ef6ce7e528e5088b07ba4760a33
John <43506685+Coniferish@users.noreply.github.com>
2026-04-03 16:02:15 -0400

    chore: lesson-01

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -3,10 +3,25 @@
 
 export default function StudentWork() {
   //add variables here
+  const name = 'John';
+  // eslint-disable-next-line
+  const age = '33';
+  const hobbies = ['drawing', 'dancing', 'exercising'];
   return (
     <div>
       {/* add JSX here */}
-      <p> Student output will go here </p>
+      {/* <p> Student output will go here </p> */}
+      <h1>About John</h1>
+      <p>
+        Hi! I'm {name}. This is my first CTD course. I've been working primarily
+        as a Python developer, though, for the past 3+ years and am looking
+        forward to doing more frontend work. Below are a few of my hobbies.
+      </p>
+      <ul>
+        {hobbies.map((hobby, i) => (
+          <li key={i}>{hobby}</li>
+        ))}
+      </ul>
     </div>
   );
 }
