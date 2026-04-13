e48752da91482954d8caa06d12fd439d7c14d4a9
John <43506685+Coniferish@users.noreply.github.com>
2026-04-13 10:39:35 -0400

    fix apostrophe

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -13,9 +13,10 @@ export default function StudentWork() {
       {/* <p> Student output will go here </p> */}
       <h1>About John</h1>
       <p>
-        Hi! I'm {name}. This is my first CTD course. I've been working primarily
-        as a Python developer, though, for the past 3+ years and am looking
-        forward to doing more frontend work. Below are a few of my hobbies.
+        Hi! I&apos;m {name}. This is my first CTD course. I&apos;ve been working
+        primarily as a Python developer, though, for the past 3+ years and am
+        looking forward to doing more frontend work. Below are a few of my
+        hobbies.
       </p>
       <ul>
         {hobbies.map((hobby, i) => (
