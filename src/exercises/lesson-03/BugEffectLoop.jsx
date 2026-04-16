f61d3924c89e2a772864dc7afc5266d3e275a4be
John <43506685+Coniferish@users.noreply.github.com>
2026-04-16 17:57:01 -0400

    exercise 1

--- src/exercises/lesson-03/BugEffectLoop.jsx
+++ src/exercises/lesson-03/BugEffectLoop.jsx
@@ -14,11 +14,16 @@ export default function BugEffectLoop() {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    setCount(count + 1);
-  });
+    setCount((count) => count + 1);
+  }, []);
 
   return <p>Bug 1 Count: {count}</p>;
 }
 
 // Explanation:
 // (Write your explanation here)
+/*
+The issue is that the effect depends on `count` and is also trying to update it. This causes an infinite loop.
+Passing an empty dependency array avoids this, making the effect only happen on mount. Also, using a function
+in the setter is safer because React will keep track of the current state of `count` and use the latest value.
+*/
