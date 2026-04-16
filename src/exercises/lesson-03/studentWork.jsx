0992f2014688a068321aa96acc2c674533da05b0
John <43506685+Coniferish@users.noreply.github.com>
2026-04-16 18:47:30 -0400

    exercise 3

--- src/exercises/lesson-03/studentWork.jsx
+++ src/exercises/lesson-03/studentWork.jsx
@@ -1,12 +1,17 @@
 //Lesson-03 Component Lifecycle, Hooks, State, and Props
 //Exercise: React Bug Hunt – Fix the broken components in this folder
 //Impport components here
+import BugEffectLoop from './BugEffectLoop';
+import BugMutatedState from './BugMutatedState';
+import BugProps from './BugProps';
 
 export default function StudentWork() {
   return (
     <div>
-      {/* add components here */}
-      <p>Student output will go here</p>
+      {/* BugEffectLoop renders 'Count: 2' when run in dev mode. In prod it'd just show 1 */}
+      <BugEffectLoop />
+      <BugMutatedState />
+      <BugProps />
     </div>
   );
 }
